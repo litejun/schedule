8eb5a398012ef9a863d1c84db87f6a6d81b5616f
litejun <litejun24@gmail.com>
2026-05-29 20:31:56 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app.jsx' // <--- 대문자 App.jsx가 아닌 소문자 app.jsx 경로로 완벽하게 일치시킵니다.
+import App from './App.jsx' // <--- 대문자 App.jsx 경로를 정확하게 매칭합니다.
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
